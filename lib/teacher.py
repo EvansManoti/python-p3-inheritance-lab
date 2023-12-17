b42eb24bb35973fbababfa187c7f81f494d35890
Evans <manotievance@gmail.com>
2023-12-17 09:18:33 +0300

    done assignment

--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,11 @@ import random
 
 class Teacher(User):
 
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "python is an easy programing language to learn",
+        ]
+
     def teach(self):
-        pass
+        return self.knowledge[random.randint(0, len(self.knowledge) - 1)]
